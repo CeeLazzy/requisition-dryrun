e4e716629dc005e1cc244f24cb36b5920fc3892a
CeeLazzy <chriscolinlazarus@gmail.com>
2026-04-14 15:28:47 +0200

    Ready for Render deployment

--- requisition.js
+++ requisition.js
@@ -414,7 +414,9 @@ Received <input name="received">
 // ==========================
 app.post("/generate-pdf", async (req, res) => {
 
-    const browser = await puppeteer.launch();
+    coconst browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
     const page = await browser.newPage();
 
     await page.goto(`http://localhost:${PORT}`, {
