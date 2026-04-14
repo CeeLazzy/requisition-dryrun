a4464dcbfc6deec4b521e57b21cfab96969ad98f
CeeLazzy <chriscolinlazarus@gmail.com>
2026-04-14 15:46:45 +0200

    Fix

--- requisition.js
+++ requisition.js
@@ -414,7 +414,7 @@ Received <input name="received">
 // ==========================
 app.post("/generate-pdf", async (req, res) => {
 
-    coconst browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
     const page = await browser.newPage();
